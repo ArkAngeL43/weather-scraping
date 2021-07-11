781e38e37944370665905de7db1ba03566f4bea2
ArkAngeL43 <87203697+ArkAngeL43@users.noreply.github.com>
2021-07-11 03:30:19 +0000

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,36 @@
+import os 
+import time 
+
+os.system(' clear ')
+A = str(input(" if you are new please type -i ==>>  "))
+
+if '-i' in A:
+       os.system(' pip install python-weather ')
+elif 'i' in A:
+         os.system(' pip install python-weather ')
+else:
+    os.system(' clear ')
+    print(" Running weather! ") 
+    time.sleep(2)  
+
+
+import os
+import python_weather
+import sys 
+
+os.system(' clear ')
+print(" ___________________________")
+print("|EX: Los Angeles California|")
+print("|__________________________|")
+print("| automation for weather   |")
+print("==================================")
+weather = str(input(" Living Area ==>>  "))
+print("==================================")
+
+os.system(f' weather {weather} ')
+
+time.sleep(2)
+print("|==================|")
+print("|Have a nice one :D|")
+print("|==================|")
+sys.exit()
